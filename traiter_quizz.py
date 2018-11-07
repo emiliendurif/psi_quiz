4a2e015f803d2b579e8a803aef70bd70494f746b
emiliendurif <emilien.durif@gmail.com>
2018-11-08 00:33:10 +0100

    maj psi_quiz

--- traiter_quizz.py
+++ traiter_quizz.py
@@ -16,13 +16,24 @@ c=conn.cursor()
 
 liste_quizz=['C2-1']
 liste_quizz=['C1-2']
-liste_quizz=[]
+liste_quizz0=['C1-2','C2-1','C3-1']#Liste des quiz réalisés dans l'ordre
+liste_quizz=['C1-2','C2-1','C3-1']#Liste des quiz à traiter pour la modification de la bdd
+
+#Mise à 0 des scores
+req_etudiants="select nom, prenom from etudiants"
+c.execute(req_etudiants)
+for l in c.fetchall():
+    (nom,prenom)=l
+    req_raz1="update etudiants set score_cumule="+str(0)+" where nom='"+nom+"' and prenom='"+prenom+"'"
+    c.execute(req_raz1)
+    req_raz2="update etudiants set classement="+str(41)+" where nom='"+nom+"' and prenom='"+prenom+"'"
+    c.execute(req_raz1)
 
 for q in liste_quizz:
     #Determination des donnees du quiz
     c.execute("SELECT idquiz, nbr_question FROM quiz WHERE nom_quiz='"+q+"'")
     (id_quiz,nbr_question)=c.fetchone()
-    path=r"/Users/emiliendurif/Documents/prepa/PSI/quiz/"+q+".xlsx"
+    path=r"/Users/emiliendurif/Documents/prepa/PSI/psi_quiz/"+q+".xlsx"
     classeur=xlrd.open_workbook(path)
     feuilles=classeur.sheet_names()
     #Ouverture de la feuille du quiz
@@ -46,46 +57,89 @@ for q in liste_quizz:
         c.execute("INSERT INTO joue (idetudiant,idquiz,score) VALUES ("+str(idetudiant[0])+","+str(id_quiz)+","+str(score)+")")
         i+=1
         #c.fetchone()
+    #Requete pour afficher le score cumule
+    req_score="select nom, prenom, sc_total from (select nom, prenom, sum(score) as sc_total from (select nom, prenom, score from etudiants join joue on etudiants.idetudiant=joue.idetudiant) group by nom) order by sc_total DESC"
+    c.execute(req_score)
+    for l in c.fetchall():
+        (nom,prenom,sc_total)=l
+        req_update="update etudiants set score_cumule="+str(sc_total)+" where nom='"+nom+"' and prenom='"+prenom+"'"
+        c.execute(req_update)
+    #Classement par rapport au score cumule
+    req="select nom, prenom, score_cumule from etudiants"
+    c.execute(req)
+    tab_quiz=c.fetchall()
+    tab_quiz.sort(key=lambda colonnes: colonnes[2])
+    tab_quiz=np.flipud(tab_quiz)
+    rank=1
+    sc_quiz=[int(tab_quiz[0,2])]
+    cl_quiz=[rank]
+    for l in tab_quiz[1:]:
+        #print(l[2],sc_quiz[-1])
+        if int(l[2])==sc_quiz[-1]:
+            cl_quiz.append(cl_quiz[-1])
+            rank+=1
+        else: 
+            rank+=1
+            cl_quiz.append(rank)
+        sc_quiz.append(int(l[2]))
+    tab_quiz=np.array(np.transpose(np.concatenate((np.mat(tab_quiz[:,0]),np.mat(tab_quiz[:,1]),np.mat(tab_quiz[:,2]),np.mat(np.array(cl_quiz))),axis=0)))
+    tab_quiz2=[]
+    for l in tab_quiz:
+        c.execute("select classement from etudiants where nom='"+l[0]+"' and prenom='"+l[1]+"'")
+        (classement_0,)=c.fetchone()
+        # req_update="update etudiants set score_cumule="+str(sc_total)+" where nom='"+l[0]+"' and prenom='"+l[1]+"'"
+        # c.execute(req_update)
+        req_update="update etudiants set classement="+str(l[3])+" where nom='"+l[0]+"' and prenom='"+l[1]+"'"
+        c.execute(req_update)
+        req_update="update etudiants set evol_classement="+str(-int(l[3])+int(classement_0))+" where nom='"+l[0]+"' and prenom='"+l[1]+"'"
+        c.execute(req_update)
+        
 conn.commit()
 conn.close()
 
-req_score="select nom, prenom, sc_total from (select nom, prenom, sum(score) as sc_total from (select nom, prenom, score from etudiants join joue on etudiants.idetudiant=joue.idetudiant) group by nom) order by sc_total DESC"
 
 
-#Post-traitement
-
+# #Post-traitement
+# ###########@
 conn=sqlite3.connect('psiii_quizz.db')
 c=conn.cursor()
-c.execute(req_score)
+# 
+# #Generation du tableau html
+# 
+c.execute("select prenom, score_cumule, classement, evol_classement from etudiants")
 data=c.fetchall()
+data.sort(key=lambda colonnes: colonnes[2])
 tableau=[]
 for ligne in data:
     tableau.append(list(ligne))
 texte='<table border="1" class="dataframe">\n  <thead>\n    <tr style="text-align: center;"></thead>\n'
-texte+='<th>Nom</th>\n<th>Prénom</th>\n<th>Score</th>\n</tr>\n'
+texte+='<th>Prénom</th>\n<th>Score</th>\n<th>Classement</th>\n<th>Evolution au classement</th>\n</tr>\n'
 i=0
 while i<=20:
     l=tableau[i]
-    texte+='<th>'+l[0]+'</th>\n<th>'+l[1]+'</th>\n<th>'+str(l[2])+'</th>\n</tr>\n'
+    if l[3]>=0:
+        texte+='<th>'+l[0]+'</th>\n<th>'+str(l[1])+'</th>\n<th>'+str(l[2])+'</th>\n<th>+'+str(l[3])+'</th>\n</tr>\n'
+    else:
+        texte+='<th>'+l[0]+'</th>\n<th>'+str(l[1])+'</th>\n<th>'+str(l[2])+'</th>\n<th>'+str(l[3])+'</th>\n</tr>\n'
     i+=1
 
 texte+='</tbody>\n</table>'  
 with open('tableau1.html','w',encoding='iso-8859-1') as f:
     f.write(texte)
-    
-texte='<table border="1" class="dataframe">\n  <thead>\n    <tr style="text-align: center;"></thead>\n'
-texte+='<th>Nom</th>\n<th>Prénom</th>\n<th>Score</th>\n</tr>\n'
-i=21
-while i<=40:
-    l=tableau[i]
-    texte+='<th>'+l[0]+'</th>\n<th>'+l[1]+'</th>\n<th>'+str(l[2])+'</th>\n</tr>\n'
-    i+=1
-
-texte+='</tbody>\n</table>'  
-with open('tableau2.html','w',encoding='iso-8859-1') as f:
-    f.write(texte)
-
-
-      
-                    
+#     
+# texte='<table border="1" class="dataframe">\n  <thead>\n    <tr style="text-align: center;"></thead>\n'
+# texte+='<th>Nom</th>\n<th>Prénom</th>\n<th>Score</th>\n</tr>\n'
+# i=21
+# while i<=40:
+#     l=tableau[i]
+#     texte+='<th>'+l[0]+'</th>\n<th>'+l[1]+'</th>\n<th>'+str(l[2])+'</th>\n</tr>\n'
+#     i+=1
+# 
+# texte+='</tbody>\n</table>'  
+# with open('tableau2.html','w',encoding='iso-8859-1') as f:
+#     f.write(texte)
+# 
+# 
+#       
+#                     
 
